6aedc5bd080435b420bbcfa0d177748e7d37ce25
Kuo Lu <kuolu@pm.me>
2026-04-18 17:00:05 -0700

    improve bash command

--- run.bash
+++ run.bash
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
 rm -rf build/ install/ log/
 colcon build
+
+# spin off a new terminal
+gnome-terminal --tab -- bash -lc 'ros2 launch turtlebot4_ignition_bringup turtlebot4_ignition.launch.py nav2:=true slam:=false localization:=true rviz:=true'
+sleep 15
+
 source install/setup.bash
 ros2 run patrol patrol_loop
--- src/patrol/patrol/patrol_loop.py
+++ src/patrol/patrol/patrol_loop.py
@@ -155,104 +155,103 @@ def main(args=None):
     rclpy.init(args=args)
     lock = Lock()
     battery_monitor = BatteryMonitor(lock)
-    try:
-        # Temporary node only for reading ROS parameters
-        param_node = Node("navigator_config_loader")
-        param_node.declare_parameter("config", "./params.yml")
-        config_path = (
-            param_node.get_parameter("config").get_parameter_value().string_value
-        )
-        param_node.get_logger().info(f"Using config file: {config_path}")
-        param_node.destroy_node()
+    # Temporary node only for reading ROS parameters
+    param_node = Node("navigator_config_loader")
+    param_node.declare_parameter("config", "./params.yml")
+    config_path = (
+        param_node.get_parameter("config").get_parameter_value().string_value
+    )
+    param_node.get_logger().info(f"Using config file: {config_path}")
+    param_node.destroy_node()
 
-        navigator = TurtleBot4Navigator()
-        battery_percent = None
-        position_index = 0
+    navigator = TurtleBot4Navigator()
+    battery_percent = None
+    position_index = 0
 
-        thread = Thread(target=battery_monitor.thread_function, daemon=True)
-        thread.start()
+    thread = Thread(target=battery_monitor.thread_function, daemon=True)
+    thread.start()
 
-        # Start on dock
-        if not navigator.getDockedStatus():
-            navigator.info("Docking before intialising pose")
-            navigator.dock()
+    # Start on dock
+    if not navigator.getDockedStatus():
+        navigator.info("Docking before intialising pose")
+        navigator.dock()
 
-        # Set initial pose
-        initial_pose = navigator.getPoseStamped([0.0, 0.0], TurtleBot4Directions.NORTH)
-        navigator.setInitialPose(initial_pose)
+    # Set initial pose
+    initial_pose = navigator.getPoseStamped([0.0, 0.0], TurtleBot4Directions.NORTH)
+    navigator.setInitialPose(initial_pose)
 
-        # Wait for Nav2
-        navigator.waitUntilNav2Active()
+    # Wait for Nav2
+    navigator.waitUntilNav2Active()
 
-        # Undock
-        navigator.undock()
+    # Undock
+    navigator.undock()
 
-        # configure goals and battery
-        config = load_navigation_config(config_path, navigator)
-        battery_high = config["battery_high"]
-        battery_low = config["battery_low"]
-        battery_critical = config["battery_critical"]
-        goal_pose = config["goal_poses"]
-        navigator.info(
-            "Parsed config:\n"
-            f"  battery_high: {battery_high:.2f}\n"
-            f"  battery_low: {battery_low:.2f}\n"
-            f"  battery_critical: {battery_critical:.2f}\n"
-            f"  goal_count: {len(goal_pose)}"
-        )
+    # configure goals and battery
+    config = load_navigation_config(config_path, navigator)
+    battery_high = config["battery_high"]
+    battery_low = config["battery_low"]
+    battery_critical = config["battery_critical"]
+    goal_pose = config["goal_poses"]
+    navigator.info(
+        "Parsed config:\n"
+        f"  battery_high: {battery_high:.2f}\n"
+        f"  battery_low: {battery_low:.2f}\n"
+        f"  battery_critical: {battery_critical:.2f}\n"
+        f"  goal_count: {len(goal_pose)}"
+    )
 
-        while True:
-            with lock:
-                battery_percent = battery_monitor.battery_percent
+    while True:
+        with lock:
+            battery_percent = battery_monitor.battery_percent
 
-            if battery_percent is not None:
-                navigator.info(f"Battery is at {(battery_percent*100):.2f}% charge")
+        if battery_percent is not None:
+            navigator.info(f"Battery is at {(battery_percent*100):.2f}% charge")
 
-                # Check battery charge level
-                if battery_percent < battery_critical:
-                    navigator.error("Battery critically low. Charge or power down")
+            # Check battery charge level
+            if battery_percent < battery_critical:
+                navigator.error("Battery critically low. Charge or power down")
+                break
+            elif battery_percent < battery_low:
+                # Go near the dock
+                navigator.info("Docking for charge")
+                navigator.startToPose(
+                    navigator.getPoseStamped([-1.0, 1.0], TurtleBot4Directions.EAST)
+                )
+                navigator.dock()
+
+                if not navigator.getDockedStatus():
+                    navigator.error("Robot failed to dock")
                     break
-                elif battery_percent < battery_low:
-                    # Go near the dock
-                    navigator.info("Docking for charge")
-                    navigator.startToPose(
-                        navigator.getPoseStamped([-1.0, 1.0], TurtleBot4Directions.EAST)
-                    )
-                    navigator.dock()
 
-                    if not navigator.getDockedStatus():
-                        navigator.error("Robot failed to dock")
-                        break
+                # Wait until charged
+                navigator.info("Charging...")
+                battery_percent_prev = 0
+                while battery_percent < battery_high:
+                    sleep(15)
+                    battery_percent_prev = floor(battery_percent * 100) / 100
+                    with lock:
+                        battery_percent = battery_monitor.battery_percent
 
-                    # Wait until charged
-                    navigator.info("Charging...")
-                    battery_percent_prev = 0
-                    while battery_percent < battery_high:
-                        sleep(15)
-                        battery_percent_prev = floor(battery_percent * 100) / 100
-                        with lock:
-                            battery_percent = battery_monitor.battery_percent
+                    # Print charge level every time it increases a percent
+                    if battery_percent > (battery_percent_prev + 0.01):
+                        navigator.info(
+                            f"Battery is at {(battery_percent*100):.2f}% charge"
+                        )
 
-                        # Print charge level every time it increases a percent
-                        if battery_percent > (battery_percent_prev + 0.01):
-                            navigator.info(
-                                f"Battery is at {(battery_percent*100):.2f}% charge"
-                            )
+                # Undock
+                navigator.undock()
+                position_index = 0
 
-                    # Undock
-                    navigator.undock()
+            else:
+                # Navigate to next position
+                navigator.startToPose(goal_pose[position_index])
+
+                position_index = position_index + 1
+                if position_index >= len(goal_pose):
                     position_index = 0
 
-                else:
-                    # Navigate to next position
-                    navigator.startToPose(goal_pose[position_index])
-
-                    position_index = position_index + 1
-                    if position_index >= len(goal_pose):
-                        position_index = 0
-    finally:
-        battery_monitor.destroy_node()
-        rclpy.shutdown()
+    battery_monitor.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
